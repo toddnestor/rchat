e7ffef2cea4aba03bc14a2a963bb6c26489c2779
Todd Nestor <todd.nestor@gmail.com>
2016-10-23 20:31:35 -0700

    well gotta disable the forgery protection then...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,10 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ '*' ]
+  config.action_cable.url = 'wss://rrrchat.herokuapp.com/cable'
+  config.disable_request_forgery_protection = true
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = [ 'http://*.rchat.us' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
